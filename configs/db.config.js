da3a207823c71605b62c18a44a14310b3489792b
Aradhya Gupta <aradhyagupta445@gmail.com>
2023-02-14 13:21:02 +0530

    adding Db

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     DB_Name: "MBA_db",
-    DB_URL: "mongodb://localhost:27017/MBA_db"
+    DB_URL: "mongodb+srv://vaibhavpika:uinSEmNbqj.D7E-@cluster0.tc42do9.mongodb.net/?retryWrites=true&w=majority"
 }
